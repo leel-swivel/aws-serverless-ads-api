1cb630dbf6f93fbb892e3a60acdf94e9eea5f87a
leel-swivel <leel@swivelgroup.com.au>
2026-03-01 12:20:46 +0530

    add s3 upload and push SNS

--- src/handlers/createAd.ts
+++ src/handlers/createAd.ts
@@ -5,15 +5,23 @@ import { apiHandler } from "../utils/handlerWrapper";
 import { createAd } from "../services/ad.service";
 import { HTTP_STATUS } from "../constants/httpStatus";
 import { validate } from "../utils/validate";
+import { AppError } from "../utils/errors";
 
+export const handler = apiHandler(async (event: APIGatewayProxyEvent) => {
+  const userId = event.requestContext.authorizer?.claims?.sub;
 
-
-export const handler = apiHandler(
-  async (event: APIGatewayProxyEvent) => {
-
-    const requestId = event.requestContext.requestId;
-    const data = validate(createAdSchema, event.body);
-    const ad = await createAd(data);
-    return success(ad, requestId, HTTP_STATUS.CREATED);
+  if (!userId) {
+    throw new AppError("Unauthorized", 401);
   }
-);
+
+  const requestId = event.requestContext.requestId;
+
+  const data = validate(createAdSchema, event.body);
+
+  const ad = await createAd({
+    ...data,
+    userId,
+  });
+
+  return success(ad, requestId, HTTP_STATUS.CREATED);
+});
