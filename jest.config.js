04645353bb453b2665e137d5e8d294cd9eb12825
leel-swivel <leel@swivelgroup.com.au>
2026-03-02 13:56:25 +0530

    Add Test coverage

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,14 @@
 module.exports = {
-    preset: "ts-jest",
-    testEnvironment: "node",
-    testMatch: ["**/?(*.)+(spec|test).ts"],
-  };
+  preset: "ts-jest",
+  testEnvironment: "node",
+  testMatch: ["**/?(*.)+(spec|test).ts"],
+
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "src/**/*.ts",
+    "!src/**/*.d.ts",
+    "!src/**/index.ts"
+  ],
+  coverageDirectory: "coverage",
+  coverageReporters: ["text", "lcov"]
+};
